0e70e46406fea55d86b132bdb0c3e0e228c5ac38
Ukiler2000 <ukiler@list.ru>
2024-12-27 01:27:20 +0300

    Изменение App.js

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
      <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
          {greeting}
+	 <p>Это новый абзац, добавленный после приветствия.</p>
       </header>
     </div>
   );
